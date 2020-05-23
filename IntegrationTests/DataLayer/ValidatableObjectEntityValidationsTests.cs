a2225313672d71fb787a08bb705608e8258dcb3d
Robert Haken <haken@havit.cz>
2020-05-23 13:56:32 +0200

    ValidatableObjectEntityValidationsTests

---
+++ IntegrationTests/DataLayer/ValidatableObjectEntityValidationsTests.cs
@@ -0,0 +1,37 @@
+﻿using Havit.Data.EntityFrameworkCore.Patterns.UnitOfWorks.EntityValidation;
+using Havit.Data.Patterns.UnitOfWorks;
+using Havit.GoranG3.Model.Finance;
+using Havit.GoranG3.TestHelpers;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.GoranG3.IntegrationTests.DataLayer
+{
+	[TestClass]
+	public class ValidatableObjectEntityValidationsTests : IntegrationTestBase
+	{
+		[TestMethod]
+		[ExpectedException(typeof(ValidationFailedException))]
+		public void ValidatableObjectEntityValidations_FailsForInvalidEntity()
+		{
+			// arrange
+			var unitOfWork = ServiceProvider.GetRequiredService<IUnitOfWork>();
+
+			var invalidEntity = new Transaction()
+			{
+				TransactionStatus = TransactionStatus.Regular
+			};
+
+			// act
+			unitOfWork.AddForInsert(invalidEntity);
+			unitOfWork.Commit();
+
+			// assert - exception expected
+		}
+	}
+}
