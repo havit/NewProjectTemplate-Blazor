c03c9aced6284f929c46435383e549cd764a9a5b
Jiří Kanda <kanda@havit.cz>
2020-05-27 12:11:51 +0200

    Filtrování faktur - zprovoznění bindování dat filtru + filtrování faktur

--- Web.Client/Pages/Prototyping/InvoiceList.razor.cs
+++ Web.Client/Pages/Prototyping/InvoiceList.razor.cs
@@ -1,4 +1,6 @@
-﻿using Havit.Blazor.Components.Web.Bootstrap.NamedViews;
+﻿using Havit.Blazor.Components.Web.Bootstrap.Filters;
+using Havit.Blazor.Components.Web.Bootstrap.NamedViews;
+using Havit.Linq;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
@@ -8,19 +10,9 @@ using System.Threading.Tasks;
 
 namespace Havit.GoranG3.Web.Client.Pages.Prototyping
 {
-	public partial class InvoiceList
-	{
-		protected List<InvoiceDto> Invoices { get; set; } = Enumerable.Range(0, 250)
-			.Select(i => new InvoiceDto
-			{
-				InvoiceId = i,
-				InvoiceNumber = $"{i}/2000",
-				IssuedDate = new DateTime(2020, 1, 1).AddDays(3 * i),
-				TaxDate = new DateTime(2020, 1, 1).AddDays(3 * i),
-				BusinessPartnerName = $"Zákazník {i}",
-				Description = "Provoz aplikací v Azure"
-			})
-			.ToList();
+    public partial class InvoiceList
+    {
+		protected List<InvoiceDto> Invoices { get; set; }
 
 		protected InvoiceDto CurrentInvoice { get; set; }
 		protected InvoiceListFilterDto Filter { get; set; } = new InvoiceListFilterDto();
@@ -45,8 +37,36 @@ namespace Havit.GoranG3.Web.Client.Pages.Prototyping
 			return Task.CompletedTask;
 		}
 
-		protected Task FilterChanged()
+		protected override void OnInitialized()
 		{
+			base.OnInitialized();
+			LoadInvoices();
+		}
+
+		private void LoadInvoices()
+		{
+			Invoices = Enumerable.Range(0, 250)
+				.Select(i => new InvoiceDto
+				{
+					InvoiceId = i,
+					InvoiceNumber = $"{i}/2000",
+					IssuedDate = new DateTime(2020, 1, 1).AddDays(3 * i),
+					TaxDate = new DateTime(2020, 1, 1).AddDays(3 * i),
+					BusinessPartnerName = $"Zákazník {i}",
+					Description = "Provoz aplikací v Azure " + (new DateTime(2020, 1, 1).AddDays(3 * i)).ToString("MM/yyyy")
+				})
+				.WhereIf(Filter.IssuedDateRange?.StartDate != null, invoice => invoice.IssuedDate >= Filter.IssuedDateRange.StartDate)
+				.WhereIf(Filter.IssuedDateRange?.EndDate != null, invoice => invoice.IssuedDate <= Filter.IssuedDateRange.EndDate)
+				.WhereIf(Filter.TaxDateRange?.StartDate != null, invoice => invoice.TaxDate >= Filter.TaxDateRange.StartDate)
+				.WhereIf(Filter.TaxDateRange?.EndDate != null, invoice => invoice.TaxDate <= Filter.TaxDateRange.EndDate)
+				.WhereIf(!String.IsNullOrEmpty(Filter.Text), invoice => invoice.Description.Contains(Filter.Text, StringComparison.CurrentCultureIgnoreCase))
+				.ToList();
+			StateHasChanged();
+		}
+
+		protected Task ApplyFilterRequested()
+		{
+			LoadInvoices();
 			// Tady by bylo něco jako BindData()
 			return Task.CompletedTask;
 		}
