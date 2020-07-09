ee6494c79ca59e108468f656c380887ed0b2b075
García, Rubén <garcia@havit.cz>
2020-07-09 15:26:36 +0100

    #50639 EmploymentTypeDataSeed, EmploymentTerms - default values

---
+++ DataLayer/Seeds/Core/HumanResources/EmploymentTerms.cs
@@ -0,0 +1,47 @@
+﻿using Havit.Data.Patterns.DataSeeds;
+using Havit.GoranG3.DataLayer.Repositories.HumanResources;
+using Havit.GoranG3.Model.HumanResources;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.GoranG3.DataLayer.Seeds.Core.HumanResources
+{
+	public class EmploymentTermsSeed : DataSeed<CoreProfile>
+	{
+		private readonly IEmploymentTermsRepository employmentTermsRepository;
+
+		public EmploymentTermsSeed(IEmploymentTermsRepository employmentTermsRepository)
+		{
+			this.employmentTermsRepository = employmentTermsRepository;
+		}
+
+		public override void SeedData()
+		{
+			if (employmentTermsRepository.GetAll().Any())
+			{
+				return; // one-off seed of defaults
+			}
+
+			var employmentTerms = new[]
+			{
+				new EmploymentTerms()
+				{
+					Name = "hodinové odměňování",
+					RateType = EmployeeRateType.HourRate,
+					HoursPerDay = 8
+				},
+				new EmploymentTerms()
+				{
+					Name = "plný úvazek",
+					RateType = EmployeeRateType.MonthRate,
+					HoursPerDay = 8
+				}
+			};
+
+			Seed(For(employmentTerms).PairBy(at => at.Name).WithoutUpdate());
+		}
+	}
+}
