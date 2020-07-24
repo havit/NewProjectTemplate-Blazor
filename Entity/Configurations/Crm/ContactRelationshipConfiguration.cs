2e1f3ad91ee495274777501da10d25fb973b7c04
García, Rubén <garcia@havit.cz>
2020-07-24 17:16:39 +0100

    #50881 Create a unique index on ContactRelationship

---
+++ Entity/Configurations/Crm/ContactRelationshipConfiguration.cs
@@ -0,0 +1,19 @@
+﻿using Havit.GoranG3.Model.Crm;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.GoranG3.Entity.Configurations.Crm
+{
+	public class ContactRelationshipConfiguration : IEntityTypeConfiguration<ContactRelationship>
+	{
+		public void Configure(EntityTypeBuilder<ContactRelationship> builder)
+		{
+			builder.HasIndex(ContactRelationship => new { ContactRelationship.ParentContactId, ContactRelationship.DetailContactId }).IsUnique();
+		}
+	}
+}
