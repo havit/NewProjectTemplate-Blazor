7c663eae70d2bf535ee97646c72a37ebfc8c3833
Robert Haken <haken@havit.cz>
2021-01-14 14:09:23 +0100

    drop WebAPI

--- WebAPI/Infrastructure/Security/JwtBearerSettings.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Havit.GoranG3.WebAPI.Infrastructure.Security
-{
-    public class JwtBearerSettings
-    {
-        public string Audience { get; set; }
-
-        public string Authority { get; set; }
-    }
-}
