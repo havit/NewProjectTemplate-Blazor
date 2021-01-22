eda3043462351d0796e3ecc2bd06f2f39a6cfc7d
Robert Haken <haken@havit.cz>
2021-01-22 11:36:31 +0100

    fixes #53522: Routes - centrální správa? náhrada GetNavigationUrl()?

---
+++ Web.Client/Routes.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Havit.GoranG3.Web.Client
+{
+	public static class Routes
+	{
+		public static string Home = "/";
+
+		public static class Administration
+		{
+			public const string Index = "/admin/";
+		}
+
+		public static class UserAdministration
+		{
+			public const string Currencies = "/admin/user/currencies";
+			public const string ExchangeRates = "/admin/user/exchange-rates";
+			public const string BankAccounts = "/admin/user/bank-accounts";
+		}
+	}
+}
