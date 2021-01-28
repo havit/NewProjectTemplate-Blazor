f088305eb763c09ec94ef7e77c4c7dfde6a3bd36
Robert Haken <haken@havit.cz>
2021-01-28 10:53:54 +0100

    přechod na Havit.SourceGenerators.StrongApiStringLocalizers

--- Web.Client/Resources/Pages/Admin/IAdminIndexLocalizer.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.Extensions.Localization;
-
-namespace Havit.GoranG3.Web.Client.Resources.Pages.Admin
-{
-	public interface IAdminIndexLocalizer : IStringLocalizer<AdminIndexLocalizer>
-	{
-		LocalizedString CultureRemoved { get; }
-	}
-}
