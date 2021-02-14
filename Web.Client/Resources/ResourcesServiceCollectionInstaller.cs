dd3e4634e9d89155de6368944ee67ce99e6edeef
Robert Haken <haken@havit.cz>
2021-02-14 01:48:40 +0100

    RESX cleanup

--- Web.Client/Resources/ResourcesServiceCollectionInstaller.cs
+++ Web.Client/Resources/ResourcesServiceCollectionInstaller.cs
@@ -3,8 +3,6 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Havit.NewProjectTemplate.Web.Client.Resources.Model.Finance;
-using Havit.NewProjectTemplate.Web.Client.Resources.Pages.Admin;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace Havit.NewProjectTemplate.Web.Client.Resources
