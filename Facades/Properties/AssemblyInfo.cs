22ffb58aab5621844e26b0053a257ed73b111145
Kanda, Jiří <kanda@havit.cz>
2021-06-25 10:40:27 +0200

    InternalsVisibleTo - převod z atributu do CSPROJ

--- Facades/Properties/AssemblyInfo.cs
+++ Facades/Properties/AssemblyInfo.cs
@@ -1,8 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 
-[assembly: InternalsVisibleTo("Havit.NewProjectTemplate.Facades.Tests")]
-
 namespace Havit.NewProjectTemplate.Facades.Properties
 {
 	public class AssemblyInfo
--- Services/Properties/AssemblyInfo.cs
+++ Services/Properties/AssemblyInfo.cs
@@ -2,8 +2,6 @@
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
-[assembly: InternalsVisibleTo("Havit.NewProjectTemplate.Services.Tests")]
-
 namespace Havit.NewProjectTemplate.Services.Properties
 {
 	public class AssemblyInfo
