ff2e4c90bf49ff9af990df6260c7ee2f1d57ebec
Kanda, Jiří <kanda@havit.cz>
2022-04-28 12:49:31 +0200

    Task #61234: Application Insights - Doplnit do reportování z utility (hangfire) označení utility do cloud_RoleName pro možnost odlišení v Performance - řešení duplicitního volání telemetry initializeru

--- Utility/Infrastructure/ApplicationInsights/UtilityToCloudRoleNameTelemetryInitializer.cs
+++ Utility/Infrastructure/ApplicationInsights/UtilityToCloudRoleNameTelemetryInitializer.cs
@@ -8,6 +8,9 @@ namespace Havit.NewProjectTemplate.Utility.Infrastructure.ApplicationInsights
 	/// pomocí hodnoty cloud_RoleName.
 	/// (Řeší i nejen RequestTelemetry, ale veškerá telemetrická data z Utility.)
 	/// </summary>
+	/// <remarks>
+	/// Custom ITelemetryInitializer being called multiple times: https://github.com/microsoft/ApplicationInsights-dotnet-server/issues/977
+	/// </remarks>
 	public class UtilityToCloudRoleNameTelemetryInitializer : ITelemetryInitializer
 	{
 		public void Initialize(ITelemetry telemetry)
@@ -16,7 +19,7 @@ namespace Havit.NewProjectTemplate.Utility.Infrastructure.ApplicationInsights
 			{
 				telemetry.Context.Cloud.RoleName = "utility";
 			}
-			else
+			else if (!telemetry.Context.Cloud.RoleName.EndsWith("utility")) // (zde není podtržítko, protože předchozí volání telemetry initializeru může nastavit RoleName na "utility" samotné)
 			{
 				telemetry.Context.Cloud.RoleName += "_utility";
 			}
