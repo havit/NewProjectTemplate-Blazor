7f371dd8a89657ff4ffc0f0dfb00cdca61eaaba6
Alexandr Hájek <hajek@havit.cz>
2022-07-19 13:13:02 +0200

    Update HealthCheckWriter to use System.Text.Json instead of Newtonsoft.Json

--- Web.Server/Infrastructure/HealthChecks/HealthCheckWriter.cs
+++ Web.Server/Infrastructure/HealthChecks/HealthCheckWriter.cs
@@ -1,7 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using System.Text;
+using System.Text.Json;
 using Microsoft.Extensions.Diagnostics.HealthChecks;
-using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace Havit.NewProjectTemplate.Web.Server.Infrastructure.HealthChecks;
 
@@ -9,30 +8,49 @@ public class HealthCheckWriter
 {
 	public static Task WriteResponse(HttpContext context, HealthReport result)
 	{
-		context.Response.ContentType = "application/json";
+		context.Response.ContentType = "application/json; charset=utf-8";
+
+		var options = new JsonWriterOptions
+		{
+			Indented = true
+		};
 
 		// https://docs.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-3.1#use-health-checks-routing
 
-		var json = new JObject(new JProperty("Items", result.Entries.Select(entry =>
+		using var stream = new MemoryStream();
+		using (var writer = new Utf8JsonWriter(stream, options))
 		{
-			var result = new JObject(
-			new JProperty("Key", entry.Key),
-			new JProperty("Status", entry.Value.Status.ToString()),
-			new JProperty("Duration", entry.Value.Duration.ToString()));
 
-			if (!String.IsNullOrEmpty(entry.Value.Description))
+			writer.WriteStartObject();
+			writer.WriteStartArray("Items");
+
+			foreach (var entry in result.Entries)
 			{
-				result.Add(new JProperty("Description", entry.Value.Description));
+				writer.WriteStartObject();
+
+				writer.WriteString("Key", entry.Key);
+				writer.WriteString("Status", entry.Value.Status.ToString());
+				writer.WriteString("Duration", entry.Value.Duration.ToString());
+
+				if (!string.IsNullOrEmpty(entry.Value.Description))
+				{
+					writer.WriteString("Description", entry.Value.Description);
+				}
+
+				if (!string.IsNullOrEmpty(entry.Value.Exception?.Message))
+				{
+					writer.WriteString("Exception", entry.Value.Exception.Message);
+				}
+
+				writer.WriteEndObject();
 			}
 
-			if (!String.IsNullOrEmpty(entry.Value.Exception?.Message))
-			{
-				result.Add(new JProperty("Exception", entry.Value.Exception?.Message ?? string.Empty));
-			}
+			writer.WriteEndArray();
+			writer.WriteEndObject();
+		}
 
-			return result;
-		})));
+		var json = Encoding.UTF8.GetString(stream.ToArray());
 
-		return context.Response.WriteAsync(json.ToString(Formatting.Indented));
+		return context.Response.WriteAsync(json);
 	}
 }
