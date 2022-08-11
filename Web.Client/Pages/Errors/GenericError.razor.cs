bc288c16a24ecf064ca811c735dfe59c333bd75e
Mikuláš Hoblík <mikuhoblik@gmail.com>
2022-08-11 14:49:35 +0200

    #59415 NewProjectTemplate - GenericError component - copy exception details to clipboard

---
+++ Web.Client/Pages/Errors/GenericError.razor.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace Havit.NewProjectTemplate.Web.Client.Pages.Errors;
+
+public partial class GenericError : IAsyncDisposable
+{
+	[Parameter] public Exception Exception { get; set; }
+	[Parameter] public EventCallback OnRecover { get; set; }
+
+	[Inject] private IJSRuntime JSRuntime { get; set; }
+
+	private IJSObjectReference jsModule;
+
+	private async Task EnsureJsModule()
+	{
+		jsModule ??= await JSRuntime.ImportModuleAsync("./js/GenericError.js");
+	}
+
+	private async Task CopyExceptionDetailsToClipboard()
+	{
+		await EnsureJsModule();
+
+		string clipboardText = Exception.ToString();
+		await jsModule.InvokeVoidAsync("copyToClipboard", clipboardText);
+	}
+
+	public async ValueTask DisposeAsync()
+	{
+		if (jsModule is not null)
+		{
+			await jsModule.DisposeAsync();
+		}
+	}
+}
---
+++ Web.Client/wwwroot/js/GenericError.js
@@ -0,0 +1,3 @@
+﻿export function copyToClipboard(text) {
+	navigator.clipboard.writeText(text);
+}
