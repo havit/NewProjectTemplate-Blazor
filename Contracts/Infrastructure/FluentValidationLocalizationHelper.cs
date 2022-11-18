fb42b798bb2b9ec7959a49f18fa0a4877b6470ca
Robert Haken <haken@havit.cz>
2022-11-18 16:39:58 +0100

    FluentValidation custom default messages from resx

---
+++ Contracts/Infrastructure/FluentValidationLocalizationHelper.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+using FluentValidation;
+using FluentValidation.Resources;
+using Microsoft.Extensions.Localization;
+
+namespace Havit.NewProjectTemplate.Contracts.Infrastructure;
+public static class FluentValidationLocalizationHelper
+{
+	public static void RegisterDefaultValidationMessages(IStringLocalizer localizer)
+	{
+		ValidatorOptions.Global.LanguageManager = new CustomFluentValidationLanguageManager(localizer);
+	}
+
+	private class CustomFluentValidationLanguageManager : LanguageManager
+	{
+		public CustomFluentValidationLanguageManager(IStringLocalizer localizer)
+		{
+			var currentCulture = CultureInfo.CurrentUICulture;
+
+			while (currentCulture != CultureInfo.InvariantCulture) // We can use this as a stop-condition, FluentValidation does not allow registering invariant translations
+			{
+				foreach (var message in localizer.GetAllStrings(includeParentCultures: true))
+				{
+					AddTranslation(currentCulture.Name, message.Name, message.Value);
+				}
+
+				currentCulture = currentCulture.Parent;
+			}
+		}
+	}
+}
---
+++ Web.Client/App.razor.cs
@@ -0,0 +1,16 @@
+using Havit.NewProjectTemplate.Contracts.Infrastructure;
+using Havit.NewProjectTemplate.Resources;
+using Microsoft.AspNetCore.Components;
+
+namespace Havit.NewProjectTemplate.Web.Client;
+
+public partial class App
+{
+	[Inject] protected IFluentValidationDefaultMessagesLocalizer ValidationMessagesLocalizer { get; set; }
+
+	protected override void OnInitialized()
+	{
+		// we cannot use IStringLocalizer in application startup class (locks the culture to the invariant one)
+		FluentValidationLocalizationHelper.RegisterDefaultValidationMessages(this.ValidationMessagesLocalizer);
+	}
+}
