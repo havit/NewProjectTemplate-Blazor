1b1381c0272d53a18134f08c504a288a070ff345
Robert Haken <haken@havit.cz>
2024-03-01 15:32:04 +0100

    LoginDisplay simplification (ReturnUrl updating not needed for most cases)

--- Web.Client/Shared/LoginDisplay.razor.cs
+++ Web.Client/Shared/LoginDisplay.razor.cs
@@ -4,26 +4,9 @@ using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
 
 namespace Havit.NewProjectTemplate.Web.Client.Shared;
 
-public partial class LoginDisplay : IDisposable
+public partial class LoginDisplay
 {
 	[Parameter] public bool ShowOnlyInitials { get; set; }
-
-	[Inject] protected NavigationManager NavigationManager { get; set; }
-
-	private string currentUrl;
-
-	// TODO: Dořešit <form> vs. HxDropdownMenu/HxDropdownItem
-	protected override void OnInitialized()
-	{
-		currentUrl = NavigationManager.ToBaseRelativePath(NavigationManager.Uri);
-		NavigationManager.LocationChanged += OnLocationChanged;
-	}
-
-	private void OnLocationChanged(object sender, LocationChangedEventArgs e)
-	{
-		currentUrl = NavigationManager.ToBaseRelativePath(e.Location);
-		StateHasChanged();
-	}
 
 	/// <summary>
 	/// Takes an email or name of the user and returns the user's initials.
@@ -54,10 +37,5 @@ public partial class LoginDisplay : IDisposable
 		}
 
 		return (names[0][0].ToString() + names[^1][0].ToString()).ToUpper();
-	}
-
-	public void Dispose()
-	{
-		NavigationManager.LocationChanged -= OnLocationChanged;
 	}
 }
