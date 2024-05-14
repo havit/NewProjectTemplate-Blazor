f74b7fd112623c3a804e04bebb66d6bbbe6f6902
Jiří Kanda <kanda@havit.cz>
2024-05-14 12:14:08 +0200

    Kosmetika konfigurace (klíč AzureKeyVault) a App.razor.cs (odebrány již nepoužívané property)

--- Web.Server/App.razor.cs
+++ Web.Server/App.razor.cs
@@ -1,13 +1,9 @@
-﻿using Microsoft.ApplicationInsights.Extensibility;
-using Microsoft.AspNetCore.Components;
+﻿using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using Microsoft.Extensions.Options;
 
 namespace Havit.NewProjectTemplate.Web.Server;
 public partial class App
 {
-	[Inject] protected IOptions<TelemetryConfiguration> TelemetryConfigurationOptions { get; set; }
-	[Inject] protected IHttpContextAccessor HttpContextAccessor { get; set; }
 	[Inject] protected IFileVersionProvider FileVersionProvider { get; set; }
 
 	// source: https://learn.microsoft.com/en-us/answers/questions/1493923/asp-append-version-true-in-blazor-net-8
