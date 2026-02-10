dbdd5f2169774659e98830b0b267b4d4f6138197
Goffa, Martin <goffa@havit.cz>
2026-02-10 14:14:47 +0100

    added OpenTelemetry.Instrumentation.GrpcNetClient To ServiceDefaults

--- ServiceDefaults/Extensions.cs
+++ ServiceDefaults/Extensions.cs
@@ -69,7 +69,7 @@ public static class Extensions
 							&& !context.Request.Path.StartsWithSegments(AlivenessEndpointPath)
 					)
 					// Uncomment the following line to enable gRPC instrumentation (requires the OpenTelemetry.Instrumentation.GrpcNetClient package)
-					//.AddGrpcClientInstrumentation()
+					.AddGrpcClientInstrumentation()
 					.AddHttpClientInstrumentation();
 			});
 
